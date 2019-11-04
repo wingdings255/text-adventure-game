ca0fb3fc9a61abd165047e977cf5f5349ce6e3a8
wingdings255 <wingdings255@gmail.com>
2019-11-03 23:15:36 -0800

    Added some timing values to allow the player enough time to view the options

--- game.py
+++ game.py
@@ -25,6 +25,7 @@ eggs = 0
 
 global name
 
+os.system(clearcmd)
 print("Halt! Who goes there? State your name.")
 name = input("Enter your name:  ")
 print("Welcome to Audale " + name)
@@ -44,6 +45,7 @@ def start():
     print("He wreaks of dead fish so you can assume his stock cant be the best")
     print("you have" + " " + str(gold) + " " + "gold")
     print("---------------------------------------------------------------")
+    time.sleep(4)
     print(" ")
     print("1). Accept the fish")
     print("->  Costs 5 gold")
@@ -58,17 +60,20 @@ def start():
 
 
 def death():
-    global clearcmd
     global gold
     global eggs
     global name
-    os.system(clearcmd)
     print("----------------------")
+    time.sleep(1)
     print(name + " HAS DIED!!! R.I.P")
+    time.sleep(1)
     print("----------------------")
+    time.sleep(1)
     if str(eggs) >= "0":
         print("You finished with " + str(eggs) + " " + "eastereggs")
-        print("You finished with " + gold + " gold")
+        time.sleep(1)
+        print("You finished with " + str(gold) + " gold")
+    time.sleep(3)
     quit()
 
 
@@ -81,11 +86,15 @@ def sequence1_section2_yes():
     inv.append("Fish")
     gold = gold - 5
     print("<<you now have" + " " + str(gold) + " " + "gold>>")
+    time.sleep(2)
     for x in inv:
-        print("<<Your inventory is:" + " " + x + " " + ">>")
+        print("-------------------------------------------")
+        print("Your inventory is:" + " " + x )
     quests.append("Bear")
+    print("-------------------------------------------")
+    time.sleep(2)
     print("!Your quests have been updates. Visit the Tavern to see avalible quests!")
-    time.sleep(5)
+    time.sleep(6)
     sequence2_section1()
 
 
@@ -107,19 +116,31 @@ def thief_easteregg():
 
 
 def sequence2_section1():
+    global clearcmd
     os.system(clearcmd)
     print("You place your fish in your bag and continue riding through the town")
     print("As you ride through the town, you hear a loud crash and men yelling.")
     print("You correctly assume it is a tavern.")
-    print("You ride up and tether your horse to a pole and head on in")
-    tavern()
+    print("---------------------------------------------------------------------")
+    print("Would you like to enter the tavern??")
+    print("1).  Yes")
+    print("2).  No")
+    ans = input("1 or 2: ")
+    if ans == "1":
+        print("You ride up and tether your horse to a pole and head on in")
+        time.sleep(4)
+        tavern()
+    else:
+        quit()
 
 
 def tavern():
     global name
     global gold
     global quests
+    os.system(clearcmd)
     print("Welcome to the Tavern " + name + " would you like to view quests?")
+    time.sleep(1)
     print("")
     print("1). yes")
     print("")
@@ -133,11 +154,15 @@ def tavern():
         ans = input("What quest do you want to begin:   ")
         if ans == quests[1]:
             print("you have chosen the " + quests[1] + " quest")
+            time.sleep(2)
             quest_bear()
         elif ans == quests[0]:
             print("This is a test quest and does not exist")
+            time.sleep(2)
+            tavern()
         else:
             print("Quest not found")
+            time.sleep(2)
             tavern()
     elif ans == "2":
         print("What do you mean no? this is literally the only thing you can do in the tavern so far")
