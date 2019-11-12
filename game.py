547996288a9f1de6cd20f05597604a17a48039ec
wingdings255 <wingdings255@gmail.com>
2019-11-12 09:16:45 -0800

    Fixed spelling mistake

--- game.py
+++ game.py
@@ -147,7 +147,7 @@ def tavern():
     print("2.) no")
     ans = input("1 or 2: ")
     if ans == "1":
-        print("Your you have [" + str(len(quests)) + "] quests")
+        print("You have [" + str(len(quests)) + "] quests")
         questchoose()
     elif ans == "2":
         print("What do you mean no? this is literally the only thing you can do in the tavern so far")
